8d62f21e32bc1fe4e6a09a8a4dcfd773be70d301
Dolphinchair <hatten.lois@outlook.fr>
2025-10-29 15:29:41 +0100

    feat: add electron into package.json && add webpack.config.js && add public folder

---
+++ public/electron.js
@@ -0,0 +1,44 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const isDev = require('electron-is-dev');
+
+function createWindow() {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      enableRemoteModule: true
+    }
+  });
+
+  // Load the app
+  const startUrl = isDev
+    ? 'http://localhost:3001'
+    : `file://${path.join(__dirname, '../dist/index.html')}`;
+    mainWindow.loadURL(startUrl);
+
+  // Open the DevTools in development
+  if (isDev) {
+    mainWindow.webContents.openDevTools();
+  }
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+app.whenReady().then(createWindow);
+
+// Quit when all windows are closed, except on macOS.
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Area Frontend</h1>
+        <p>Welcome to your Electron + React application!</p>
+        <p>
+          This is a desktop application built with:
+        </p>
+        <ul>
+          <li>⚛️ React</li>
+          <li>⚡ Electron</li>
+          <li>📦 Webpack</li>
+        </ul>
+      </header>
+    </div>
+  );
+}
+
+export default App;
