ed55c8c345597ac2b00a7d1e30bdaf8987dad726
Dolphinchair <hatten.lois@outlook.fr>
2025-11-20 20:35:16 +0100

    feat: add login to navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -20,6 +20,12 @@ const Navbar: React.FC = () => {
           Explore
         </NavLink>
       </nav>
+      <nav style={styles.loginContainer} aria-label="Login container">
+        <NavLink to="/login" style={({ isActive }) => ({ ...styles.loginLink, opacity: isActive ? 1 : 0.85 })}>
+          <svg viewBox="0 0 24 24" fill="none" style={{width:"44px",height:"44px"}} xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M16 7C16 9.20914 14.2091 11 12 11C9.79086 11 8 9.20914 8 7C8 4.79086 9.79086 3 12 3C14.2091 3 16 4.79086 16 7Z" stroke="#fff" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path> <path d="M12 14C8.13401 14 5 17.134 5 21H19C19 17.134 15.866 14 12 14Z" stroke="#fff" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path> </g></svg>
+          <span style={styles.item}>Login</span>
+        </NavLink>
+      </nav>
     </div>
   );
 };
@@ -65,6 +71,21 @@ const styles: { [k: string]: React.CSSProperties } = {
     fontFamily: "'Montserrat', sans-serif",
     writingMode: "horizontal-tb",
   },
+  loginContainer: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    marginBottom: "50px",
+  },
+  loginLink: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    gap: "6px",
+    color: "#fff",
+    textDecoration: "none",
+  }
 };
+
 
 export default Navbar;
