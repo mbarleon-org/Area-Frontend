c2b8419c601700f064722a10de422a47528ef794
Narkowz <attilio.jaeger@gmail.com>
2025-12-03 15:55:42 +0100

    feat: merged home page for both platforms

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,12 @@
 import React from 'react';
-import ROUTES from './constants/Router';
+import Home from './pages/Home';
 
-const isWeb = typeof document !== 'undefined';
 
 const App: React.FC = () => {
-  const Router = isWeb ? require('react-router-dom').BrowserRouter : require('react-router').MemoryRouter;
-  const RoutesComp = isWeb ? require('react-router-dom').Routes : require('react-router').Routes;
-  const RouteComp = isWeb ? require('react-router-dom').Route : require('react-router').Route;
-
-  const RouterC: any = Router;
-  const RoutesAny: any = RoutesComp;
-  const RouteAny: any = RouteComp;
-
   return (
-    <RouterC>
-      <div className="bg-white">
-        <RoutesAny>
-          {ROUTES.map(({ path, element }: any) => (
-            <RouteAny key={path} path={path} element={element} />
-          ))}
-        </RoutesAny>
-      </div>
-    </RouterC>
+    <>
+      <Home />
+    </>
   );
 };
 
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,29 +1,73 @@
 import React from 'react';
 if (typeof document !== 'undefined') require('../../index.css');
-import Navbar from "../../components/Navbar";
+// import Navbar from "../../components/Navbar";
+
+const isWeb = typeof document !== 'undefined';
 
 const Home: React.FC = () => {
+  if (!isWeb) {
+    const { View, Text } = require('react-native');
+    return (
+      <>
+        {/* <Navbar /> */}
+        <View style={mobileStyles.container}>
+          <Text style={mobileStyles.title}>Welcome to the AREA Home Page</Text>
+        </View>
+      </>
+    );
+  }
   return (
     <>
-      <Navbar />
-      <div style={styles.container}>
-        <h2>Welcome to the AREA Home Page</h2>
+      {/* <Navbar /> */}
+      <div style={webStyles.container}>
+        <h2 style={webStyles.title}>Welcome to the AREA Home Page</h2>
       </div>
     </>
   );
 };
 
-const styles: { [k: string]: React.CSSProperties } = {
+const webStyles: any = {
   container: {
-    minHeight: "100vh",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
     padding: 0,
     margin: 0,
-    paddingLeft: "80px",
-    backgroundColor: "#151316ff",
-    color: "#fff",
+    width: '100vw',
+    height: '100vh',
+    boxSizing: 'border-box',
+    backgroundColor: '#151316',
+    color: '#fff',
+  },
+  title: {
+    color: '#fff',
+    fontSize: 20,
+    fontWeight: '700',
+    margin: 0,
+    textAlign: 'center' as any,
+    width: '100%'
+  },
+};
+
+const mobileStyles = {
+  container: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#151316',
+  },
+  title: {
+    color: '#fff',
+    fontSize: 20,
+    fontWeight: '700',
+    marginBottom: 12
   },
 };
 
