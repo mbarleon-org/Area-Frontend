dc22ae5b1962456fbd65b148b9e69fc1b6485f5d
Dolphinchair <hatten.lois@outlook.fr>
2026-01-13 14:54:10 +0100

    fix automation canva

--- src/pages/Automations/index.tsx
+++ src/pages/Automations/index.tsx
@@ -1,9 +1,24 @@
 import React from "react";
 if (typeof document !== 'undefined') require('../../index.css');
 import Navbar from "../../components/Navbar";
+import { isWeb } from "../../utils/IsWeb";
 import Canvas from "../../components/Canvas";
 
 const Automations: React.FC = () => {
+  // ------------------------ Mobile view ------------------------
+  if (!isWeb) {
+    const { View } = require('react-native');
+    return (
+      <>
+        <Navbar />
+        <View style={{ paddingLeft: 24, minHeight: '100%', backgroundColor: '#151316ff', flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+          <Canvas />
+        </View>
+      </>
+    );
+  }
+
+  // ------------------------ Web view ---------------------------
   return (
     <>
       <Navbar />
